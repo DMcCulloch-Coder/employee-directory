f3983581a13c10c21d6d0e06f9c3d555697c7f25
David <DMcCulloch86@gmail.com>
2020-02-19 15:41:41 -0500

    Updates

---
+++ precache-manifest.61b5779b6834a22299ef517c256ef7cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3408dfb23a21e730e402dd091ed27414",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "acd5ab9512ae51b3570e",
+    "url": "/employee-directory/static/css/main.68e4e72e.chunk.css"
+  },
+  {
+    "revision": "4c8f0ff74c3b1e6cf189",
+    "url": "/employee-directory/static/js/2.2d554764.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/employee-directory/static/js/2.2d554764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd5ab9512ae51b3570e",
+    "url": "/employee-directory/static/js/main.290d0fea.chunk.js"
+  },
+  {
+    "revision": "100984a94e34414a2f61",
+    "url": "/employee-directory/static/js/runtime-main.6cd53313.js"
+  }
+]);
--- src/utils/API.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const url = 'https://randomuser.me/api/?results=15';
-
-export default {
-    search: () => axios.get(url)
-}
